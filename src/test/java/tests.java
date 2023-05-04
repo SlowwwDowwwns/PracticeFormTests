37170625a0978fd0fe5f90c6624b047657873137
Mikhail Kurbanov <slowwwdowns@gmail.com>
2023-05-04 08:45:58 +0300

    one

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -1,8 +1,12 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.checked;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,25 +18,34 @@ public class tests {
         Configuration.holdBrowserOpen = true;
         Configuration.timeout = 10000;
         Configuration.browserSize = "1920x1080";
+    /*  $("#firstName").shouldBe(empty);
+        $("#lastName").shouldBe(empty);
+        $("#userEmail").shouldBe(empty); */
     }
-
-
     @Test
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Mike"); // "id=userName" не работает \ а "#userName" проходит???
-        $("#userEmail").setValue("mike@mike.com");
-        $("#currentAddress").setValue("Rabochaya 1a");
-        $("#permanentAddress").setValue("Rabochyi prospect");
+        open("/automation-practice-form");
+
+        $("#firstName").val("Mike");
+        $("#lastName").val("Kurbanov");
+        $("#userEmail").val("mike@mike.com");
+        $(byText("Male")).click();
+        Selenide.sleep(1000);
+        $("#userNumber").val("79165607647");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--013:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").sendKeys("Arts");
+        $("#subjectsInput").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("C:\Users\Slowdowns\IdeaProjects\PracticeTests\src\test\resources\1.png")
+        $("#currentAddress").setValue("Moscow");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Panipat")).click();
         $("#submit").click();
 
-        //   $("id=submit").click();
-        $("#output #name").shouldHave(text("Mike"));
-//        $("#output").$("#name").shouldHave(text("Mike"));
-//        $("#output").shouldHave(text("Mike"));
-//        $("#name").shouldHave(text("Mike"));
-        $("#output #email").shouldHave(text("mike@mike.com"));
-        $("#output #currentAddress").shouldHave(text("Rabochaya 1a"));
-        $("#output #permanentAddress").shouldHave(text("Rabochyi prospect"));
     }
 }
